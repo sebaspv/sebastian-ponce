383a2fbcd516c0d46907f4b7df90b9169d7c7f9b
Sebastian Ponce <poncegramming@gmail.com>
2021-12-22 04:52:07 +0000

    hello world

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div className="font-bold text-3xl flex h-screen">
+      <div className="m-auto">
+      Hello World!
+      </div>
+    </div>
+  )
+}
+
+export default index;
