1cff7a17cd99956ded327fc06639349bca46c1b8
Sebastian Ponce <poncegramming@gmail.com>
2021-12-22 05:03:11 +0000

    cool animation

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function index() {
   return (
     <div className="font-bold text-3xl flex h-screen">
-      <div className="m-auto">
+      <div className="m-auto animate-fade-in-down">
       Hello World!
       </div>
     </div>
