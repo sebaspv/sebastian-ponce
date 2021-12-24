c06ee6c4defce50193c7b71b32529ae0f66d3106
Sebastian Ponce <poncegramming@gmail.com>
2021-12-24 05:13:55 +0000

    WE NOW HAVE A BLOG WOOOOOOOOOOOOO

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,76 +1,72 @@
 import { GraphQLClient } from 'graphql-request';
-import renderToString from 'next-mdx-remote/render-to-string';
-import hydrate from 'next-mdx-remote/hydrate';
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
 import he from 'he';
+import Test from '../components/Test';
 
-import Test from "../components/Test"
-
-require('dotenv').config()
-const graphcms = GraphQLClient(
-    process.env.BLOG_API_URL
-)
+const graphcms = new GraphQLClient(
+    'https://api-us-east-1.graphcms.com/v2/ckxjlw89l386k01xp5f5s3qem/master'
+);
 
 const components = {
-    h2: (props) => <h2 style={{ color: 'red' }} {...props} />,
-    Test,
+  h2: (props) => <h2 style={{ color: 'red' }} {...props} />,
+  Test,
 };
 
 export async function getStaticProps({ params }) {
-    const { product } = await graphcms.request(
-        `
-      query ProductPageQuery($slug: String!) {
-        product(where: { slug: $slug }) {
-          name
-          content {
-            markdown
-          }
-        }
+  const { product } = await graphcms.request(
+    `
+    query ProductPageQuery($slug: String!) {
+      product(where: { slug: $slug }) {
+        name
+        content
+        price
       }
-    `,
-        {
-            slug: params.slug,
-        }
-    );
+    }
+  `,
+    {
+      slug: params.slug,
+    }
+  );
 
-    return {
-        props: {
-            product: {
-                mdx: await renderToString(
-                    he.decode(product.content.markdown),
-                    components
-                ),
-                ...product,
-            },
-        },
-    };
+  return {
+    props: {
+      product: {
+        mdx: await serialize(
+          he.decode(product.content)
+        ),
+        ...product,
+      },
+    },
+  };
 }
 
 export async function getStaticPaths() {
-    const { products } = await graphcms.request(`
-      {
-        products {
-          slug
-          name
-        }
+  const { products } = await graphcms.request(`
+    {
+      products {
+        slug
+        name
       }
-    `);
+    }
+  `);
 
-    return {
-        paths: products.map(({ slug }) => ({
-            params: { slug },
-        })),
-        fallback: false,
-    };
+  return {
+    paths: products.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
 }
 
 const ProductPage = ({ product }) => {
-    return (
-        <React.Fragment>
-            <h1>{product.name}</h1>
-            {hydrate(product.mdx, components)}
-            <p>{product.price / 100}</p>
-        </React.Fragment>
-    );
+  return (
+    <>
+      <h1>{product.name}</h1>
+      <MDXRemote {...product.mdx} components={components} />
+      <p>{product.price / 100}</p>
+    </>
+  );
 };
 
 export default ProductPage;
