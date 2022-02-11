cff1f7aac76f4b88da4f70deb79de0af83068a62
Sebastian Ponce <poncegramming@gmail.com>
2022-02-11 16:05:03 +0000

    new resume

--- components/Hero.js
+++ components/Hero.js
@@ -52,7 +52,7 @@ export const Hero = () => {
                         <button type="button" className="text-white bg-gradient-to-br from-yellow-300 to-pink-600 hover:bg-gradient-to-bl focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mt-2">Contact me!</button>
                     </Link>
                     <Link
-                        href="/resume.pdf"
+                        href="/sebastian_resume.pdf"
                         className="inline-flex items-center justify-center h-12 px-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md focus:shadow-outline focus:outline-none"
                         aria-label="Connect with me"
                         title="Connect with me"
