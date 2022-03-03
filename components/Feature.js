0a02a22ecfa38c02898b3331e48067c4ea412e4c
Sebastian Ponce <poncegramming@gmail.com>
2022-03-03 18:08:02 +0000

    finally another component on the landing page lol

---
+++ components/Feature.js
@@ -0,0 +1,134 @@
+export const Feature = () => {
+    return (
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+                <h4 className="max-w-lg font-sans text-xl font-bold leading-none tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-pink-600 sm:text-3xl md:mx-auto">
+                    Services
+                </h4>
+                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-6xl md:mx-auto">
+                    Solving problems is my motto.
+                </h2>
+                <p className="text-base text-gray-700 md:text-lg">
+                    Using modern technologies, I can always help with solving problems and tackling them with a faster and better approach.
+                </p>
+            </div>
+            <div className="grid max-w-screen-lg gap-x-8 gap-y-7 row-gap-10 mx-auto lg:grid-cols-2">
+                <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+                    <div className="mr-4">
+                        <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-gray-200">
+                            <svg
+                                className="w-10 h-10 text-deep-purple-accent-400"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                    <div>
+                        <h6 className="mb-3 text-xl font-bold leading-5">What can I do for you?</h6>
+                        <p className="mb-3 text-base text-gray-900">
+                            Working with emerging technologies is my passion. That's why I especialize
+                            in Artificial Intelligence, Data Science, and Machine Learning. However, I also
+                            love working with web frameworks for Frontend and Backend development.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+                    <div className="mr-4">
+                        <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-gray-200">
+                            <svg
+                                className="w-10 h-10 text-deep-purple-accent-400"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                    <div>
+                        <h6 className="mb-3 text-xl font-bold leading-5">
+                            What can I build for you?
+                        </h6>
+                        <p className="mb-3 text-base text-gray-900">
+                        My workflow is based on building scalable, efficient, and user-friendly products.
+                        Building and deploying services is my biggest passion, and making them as efficient
+                        and effective as possible is my biggest satisfaction.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+                    <div className="mr-4">
+                        <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-gray-200">
+                            <svg
+                                className="w-10 h-10 text-deep-purple-accent-400"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                    <div>
+                        <h6 className="mb-3 text-xl font-bold leading-5">
+                        What can you expect?
+                        </h6>
+                        <p className="mb-3 text-base text-gray-900">
+                            I love building products that solve problems, from day-to-day small problems to global impact solutions. I use modern technologies, emerging technologies, and frameworks to build these products.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+                    <div className="mr-4">
+                        <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-gray-200">
+                            <svg
+                                className="w-10 h-10 text-deep-purple-accent-400"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                    <div>
+                        <h6 className="mb-3 text-xl font-bold leading-5">
+                            What can I help you with?
+                        </h6>
+                        <p className="mb-3 text-base text-gray-900">
+                            Machine Learning and API development is my passion. However, I also work
+                            with modern Frontend development frameworks and Big Data Analysis.
+                            I use modern frameworks for these tasks, and I will always
+                            build scalable and efficient code.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Feature;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Hero from "../components/Hero"
 import NotANav from "../components/NotANav"
+import Feature from "../components/Feature"
+import Footer from "../components/Footer"
 
 const index = () => {
   return (
@@ -11,6 +13,12 @@ const index = () => {
         </div>
         <div className="w-full overflow-hidden">
           <Hero />
+        </div>
+        <div className="w-full overflow-hidden">
+          <Feature />
+        </div>
+        <div className="w-full overflow-hidden">
+          <Footer />
         </div>
       </div>
     </div>
